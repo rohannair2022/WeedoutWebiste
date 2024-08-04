55cdbdb948d8816cc7f0a1308ea41da39a1d50a5
Rohan Nair <nairrohan151@gmail.com>
2024-08-04 10:54:50 -0400

    BDD Test cases created for Landing Page

---
+++ tests/features/environment.py
@@ -0,0 +1,25 @@
+"""
+Environment for Behave Testing
+"""
+from os import getenv
+from selenium import webdriver
+
+WAIT_SECONDS = int(getenv('WAIT_SECONDS', '60'))
+BASE_URL = getenv('BASE_URL', 'http://127.0.0.1:5000/')
+
+
+def before_all(context):
+    """ Executed once before all tests """
+    context.base_url = BASE_URL
+    context.wait_seconds = WAIT_SECONDS
+    # Instantiate the Firefox WebDriver with GeckoDriver
+    options = webdriver.FirefoxOptions()
+    
+    # --headless is to prevent the GUI
+    options.add_argument("--headless")
+    context.driver = webdriver.Firefox(options=options)
+    context.driver.implicitly_wait(context.wait_seconds)
+
+def after_all(context):
+    """ Executed after all tests """
+    context.driver.quit()
